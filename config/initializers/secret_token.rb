14d2a2af4bb5df3b71be533d8699018d30614c61
excell <excell@homestudios.(none)>
2012-06-26 23:40:02 +0700

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creditcalc::Application.config.secret_token = 'fd560e7f72e72ff68c48c130f6cbf741a552a9369f22521deeea623f944264be0a14bc98dfd82278418271a824728f05413aa2141919918b89dcd344ff3b5d2a'
