ecae1a1acc592da00a5421a57c6d24ce3f6800ae
excell <excell@homestudios.(none)>
2012-06-29 00:32:40 +0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Creditcalc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'calculator#show'
 
   # See how all your routes lay out with "rake routes"
 
